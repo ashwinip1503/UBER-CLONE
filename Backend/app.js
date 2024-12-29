b94046309b39e6d444e3585bb31ef2ab088f230d
ashwinip1503 <ashwinipadwal0315@gmail.com>
2024-12-29 20:22:00 +0530

    Add user registration functionality and Postman tests

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,23 @@
+
 const express = require('express');
-const app = express();
 const dotenv = require('dotenv');
-dotenv.config();
 const cors = require('cors');
+const connectToDb = require('./db/db');
+const userRoutes = require('./Routes/user.routes');
+
+dotenv.config();
+const app = express();
+
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Welcome  ');
+    res.send('Welcome');
 });
+
+app.use('/users', userRoutes);
 
 module.exports = app;
