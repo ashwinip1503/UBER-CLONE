a65617044ccb485253768574b71bde343662cbf4
ashwinip1503 <ashwinipadwal0315@gmail.com>
2025-01-02 19:43:34 +0530

    usercontext created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Userlogin from "./pages/UserLogin";
 import Usersignup from "./pages/UserSignup";
 import Captainlogin from "./pages/Captainlogin";
-import Captainsignup from "./pages/Captainsignup";
+import Captainsignup from "./pages/CaptainSignup";
+import { UserDataContext } from "./context/UserContext";
 
 const App = () => {
+  
+  const ans = useContext(UserDataContext);
+  console.log(ans)
   return (
     <div>
       <Routes>
